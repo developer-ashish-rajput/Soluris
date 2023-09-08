02b982cf55dc12e6823c34bd0ce23e5498d5327d
nitinsingh <nitinsingh@digiryte.com>
2023-09-08 21:27:26 +0530

    section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import About from "./component/about/About";
+import Main from "./component/main-slider/Main";
+import Service from "./component/service/Service";
+import Whychooseus from "./component/whychooseus/Whychooseus";
+import Whychooseus2 from "./component/whychooseus2/Whychooseus2";
+import Work from "./component/work/Work";
 
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div >
+      <Main />
+      <About />
+      <Service />
+      <Whychooseus />
+      <Whychooseus2 />
+      <Work />
     </div>
   );
 }
