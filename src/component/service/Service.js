62a7cd55e52a89ad2bbfa9da7f07f8c0c3132d7d
Nitin Gupta <nitingupta5897@gmail.com>
2023-09-16 19:12:27 +0530

    serviceDetails and serviceList daynamic

--- src/component/service/Service.js
+++ src/component/service/Service.js
@@ -1,9 +1,45 @@
+
 import React from 'react';
-import Service1 from '../../images/service-1.jpg';
-import Service2 from '../../images/service-2.jpg';
-import Service3 from '../../images/service-3.jpg';
-import Service4 from '../../images/service-4.jpg';
 import './service.css';
+
+const serviceData = [
+  {
+    id: 1,
+    iconClass: 'flaticon-wind-energy-1',
+    subTitle: '01 Service',
+    title: 'Wind Turbines',
+    text: 'Hybrid energy refers to the use of multiple sources ...',
+    imgSrc: require('../../images/service-1.jpg').default,
+    link: 'https://kodesolution.com/html/2023/soluris-html/page-service-details.html',
+  },
+  {
+    id: 2,
+    iconClass: 'flaticon-settings-2',
+    subTitle: '02 Service',
+    title: 'Maintenance',
+    text: 'Wind turbines are devices that convert wind energy ...',
+    imgSrc: require('../../images/service-2.jpg').default,
+    link: 'https://kodesolution.com/html/2023/soluris-html/page-service-details.html',
+  },
+  {
+    id: 3,
+    iconClass: 'flaticon-windmill',
+    subTitle: '03 Service',
+    title: 'Wind Generators',
+    text: 'Wind generators are devices that harness the ...',
+    imgSrc: require('../../images/service-3.jpg').default,
+    link: 'https://kodesolution.com/html/2023/soluris-html/page-service-details.html',
+  },
+  {
+    id: 4,
+    iconClass: 'flaticon-solar-panel',
+    subTitle: '04 Service',
+    title: 'Solar PV Systems',
+    text: 'A Solar PV (photovoltaic) system is a type of ...',
+    imgSrc: require('../../images/service-4.jpg').default,
+    link: 'https://kodesolution.com/html/2023/soluris-html/page-service-details.html',
+  },
+];
 
 const Service = () => {
   return (
@@ -14,120 +50,34 @@ const Service = () => {
           <h2>Sustainable Energy Services</h2>
         </div>
         <div className='row'>
-          {/* Service Block 1 */}
-          <div
-            className='service-block col-lg-3 col-sm-6 wow fadeInUp animated'
-            style={{ visibility: 'visible', animationName: 'fadeInUp' }}
-          >
-            <div className='inner-box'>
-              <div className='content-box'>
-                <i className='icon flaticon-wind-energy-1'></i>
-                <span className='sub-title'>01 Service</span>
-                <h4 className='title'>
-                  <a href='https://kodesolution.com/html/2023/soluris-html/page-service-details.html'>Wind Turbines</a>
-                </h4>
-                <div className='text'>Hybrid energy refers to the use of multiple sources ...</div>
-                <a href='https://kodesolution.com/html/2023/soluris-html/index.html' className='read-more'>
-                  <i className='fa fa-chevron-right'></i>
-                </a>
-              </div>
-              <div className='image-box'>
-                <figure className='image'>
-                  <a href='https://kodesolution.com/html/2023/soluris-html/page-service-details.html'>
-                    <img src={Service1} alt='' />
+          {serviceData.map((service) => (
+            <div
+              key={service.id}
+              className='service-block col-lg-3 col-sm-6 wow fadeInUp animated'
+              style={{ visibility: 'visible', animationName: 'fadeInUp' }}
+            >
+              <div className='inner-box'>
+                <div className='content-box'>
+                  <i className={`icon ${service.iconClass}`}></i>
+                  <span className='sub-title'>{service.subTitle}</span>
+                  <h4 className='title'>
+                    <a href={service.link}>{service.title}</a>
+                  </h4>
+                  <div className='text'>{service.text}</div>
+                  <a href='https://kodesolution.com/html/2023/soluris-html/index.html' className='read-more'>
+                    <i className='fa fa-chevron-right'></i>
                   </a>
-                </figure>
+                </div>
+                <div className='image-box'>
+                  <figure className='image'>
+                    <a href={service.link}>
+                      <img src={service.imgSrc} alt='' />
+                    </a>
+                  </figure>
+                </div>
               </div>
             </div>
-          </div>
-
-          {/* Service Block 2 */}
-          <div
-            className='service-block col-lg-3 col-sm-6 wow fadeInUp animated'
-            data-wow-delay='200ms'
-            style={{ visibility: 'visible', animationDelay: '200ms', animationName: 'fadeInUp' }}
-          >
-            <div className='inner-box'>
-              <div className='content-box'>
-                <i className='icon flaticon-settings-2'></i>
-                <span className='sub-title'>02 Service</span>
-                <h4 className='title'>
-                  <a href='https://kodesolution.com/html/2023/soluris-html/page-service-details.html'>Maintenance</a>
-                </h4>
-                <div className='text'>Wind turbines are devices that convert wind energy ...</div>
-                <a href='https://kodesolution.com/html/2023/soluris-html/index.html' className='read-more'>
-                  <i className='fa fa-chevron-right'></i>
-                </a>
-              </div>
-              <div className='image-box'>
-                <figure className='image'>
-                  <a href='https://kodesolution.com/html/2023/soluris-html/page-service-details.html'>
-                    <img src={Service2} alt='' />
-                  </a>
-                </figure>
-              </div>
-            </div>
-          </div>
-
-          {/* Service Block 3 */}
-          <div
-            className='service-block col-lg-3 col-sm-6 wow fadeInUp animated'
-            data-wow-delay='400ms'
-            style={{ visibility: 'visible', animationDelay: '400ms', animationName: 'fadeInUp' }}
-          >
-            <div className='inner-box'>
-              <div className='content-box'>
-                <i className='icon flaticon-windmill'></i>
-                <span className='sub-title'>03 Service</span>
-                <h4 className='title'>
-                  <a href='https://kodesolution.com/html/2023/soluris-html/page-service-details.html'>
-                    Wind Generators
-                  </a>
-                </h4>
-                <div className='text'>Wind generators are devices that harness the ...</div>
-                <a href='https://kodesolution.com/html/2023/soluris-html/index.html' className='read-more'>
-                  <i className='fa fa-chevron-right'></i>
-                </a>
-              </div>
-              <div className='image-box'>
-                <figure className='image'>
-                  <a href='https://kodesolution.com/html/2023/soluris-html/page-service-details.html'>
-                    <img src={Service3} alt='' />
-                  </a>
-                </figure>
-              </div>
-            </div>
-          </div>
-
-          {/* Service Block 4 */}
-          <div
-            className='service-block col-lg-3 col-sm-6 wow fadeInUp animated'
-            data-wow-delay='600ms'
-            style={{ visibility: 'visible', animationDelay: '600ms', animationName: 'fadeInUp' }}
-          >
-            <div className='inner-box'>
-              <div className='content-box'>
-                <i className='icon flaticon-solar-panel'></i>
-                <span className='sub-title'>04 Service</span>
-                <h4 className='title'>
-                  <a href='https://kodesolution.com/html/2023/soluris-html/page-service-details.html'>
-                    Solar PV Systems
-                  </a>
-                </h4>
-                <div className='text'>A Solar PV (photovoltaic) system is a type of ...</div>
-                <a href='https://kodesolution.com/html/2023/soluris-html/index.html' className='read-more'>
-                  <i className='fa fa-chevron-right'></i>
-                </a>
-              </div>
-              <div className='image-box'>
-                <figure className='image'>
-                  <a href='https://kodesolution.com/html/2023/soluris-html/page-service-details.html'>
-                    <img src={Service4} alt='' />
-                  </a>
-                </figure>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
--- src/services/ServiceList.js
+++ src/services/ServiceList.js
@@ -5,6 +5,45 @@ import list_img2 from '../images/service-2.jpg'
 import list_img3 from '../images/service-3.jpg'
 import list_img4 from '../images/service-5.jpg'
 import { NavLink } from 'react-router-dom'
+
+const serviceData = [
+  {
+    id: 1,
+    img: list_img1,
+    title: 'Hybrid Energy',
+    Text: 'Hybrid energy refers to the use of multiple sources of energy to meet our energy needs.',
+    button: 'Read More'
+    
+  },
+
+  {
+    id: 1,
+    img: list_img2,
+    title: 'Wind Turbines',
+    Text: 'Hybrid energy refers to the use of multiple sources of energy to meet our energy needs.',
+    button: 'Read More'
+    
+  },
+
+  {
+    id: 3,
+    img: list_img3,
+    title: 'Maintenance',
+    Text: 'Wind turbines are devices that convert wind energy into electrical power.',
+    button: 'Read More'
+  },
+
+  {
+    id: 4,
+    img: list_img4,
+    title: 'Wind Generators',
+    Text: 'Wind generators are devices that harness the power of wind to generate electricity.',
+    button: 'Read More'
+  },
+
+  
+]
+
 const ServiceList = () => {
   return (
     <>
@@ -22,56 +61,23 @@ const ServiceList = () => {
     <section className="services-section-home2">
 		<div className="large-container">
       <div className="row">
-				<div className="col-xl-3 col-md-6">
+        {serviceData.map((service)=>(
+				<div key={service} className="col-xl-3 col-md-6">
           {/* <!-- Service Block Two --> */}
           <div className="service-block-home2 wow fadeInUp animated" style={{visibility: 'visible', animationName:' fadeInUp'}}>
-            <figure className="image"><NavLink to="/service-details"><img src={list_img1} alt=""/></NavLink></figure>
+            <figure className="image"><NavLink to="/service-details"><img src={service.img} alt=""/></NavLink></figure>
             <div className="inner-box ">
               <i className="icon flaticon-solar-panel"></i>
-              <h4 className="title mt-0"><NavLink to="/service-details">Hybrid Energy</NavLink></h4>
-              <div className="text">Hybrid energy refers to the use of multiple sources of energy to meet our energy needs.</div>
-              <NavLink to="/service-details" className="read-more">Read More</NavLink>
+              <h4 className="title mt-0"><NavLink to="/service-details">{service.title}</NavLink></h4>
+              <div className="text">{service.Text}</div>
+              <NavLink to="/service-details" className="read-more">{service.button}</NavLink>
             </div>
           </div>
         </div>
-				<div className="col-xl-3 col-md-6">
-          {/* <!-- Service Block Two --> */}
-          <div className="service-block-home2 wow fadeInUp animated"  style = {{visibility: 'visible',animationName: 'fadeInUp'}}>
-            <figure className="image"><NavLink to="/service-details"><img src={list_img2} alt=""/></NavLink></figure>
-            <div className="inner-box ">
-              <i className="icon flaticon-wind-turbine"></i>
-              <h4 className="title mt-0"><NavLink to="/service-details">Wind Turbines</NavLink></h4>
-              <div className="text">Hybrid energy refers to the use of multiple sources of energy to meet our energy needs.</div>
-              <NavLink to="/service-details" className="read-more">Read More</NavLink>
-            </div>
-          </div>
-        </div>
-				<div className="col-xl-3 col-md-6">
-          {/* <!-- Service Block Two --> */}
-          <div className="service-block-home2 wow fadeInUp animated"  style = {{visibility: 'visible',animationName: 'fadeInUp'}}>
-            <figure className="image"><NavLink to="/service-details"><img src={list_img3} alt=""/></NavLink></figure>
-            <div className="inner-box ">
-              <i className="icon flaticon-settings"></i>
-              <h4 className="title mt-0"><NavLink to="/service-details">Maintenance</NavLink></h4>
-              <div className="text">Wind turbines are devices that convert wind energy into electrical power.</div>
-              <NavLink to="/service-details" className="read-more">Read More</NavLink>
-            </div>
-          </div>
-        </div>
-				<div className="col-xl-3 col-md-6">
-          {/* <!-- Service Block Two --> */}
-          <div className="service-block-home2 wow fadeInUp animated" style = {{visibility: 'visible',animationName: 'fadeInUp'}}>
-            <figure className="image"><NavLink to="/service-details"><img src={list_img4} alt=""/></NavLink></figure>
-            <div className="inner-box ">
-              <i className="icon flaticon-windmill"></i>
-              <h4 className="title mt-0"><NavLink to="/service-details">Wind Generators</NavLink></h4>
-              <div className="text">Wind generators are devices that harness the power of wind to generate electricity.</div>
-              <NavLink to="/service-details" className="read-more">Read More</NavLink>
-            </div>
-          </div>
-        </div>
-      </div>
+        ))}     
+     </div>
 		</div>
+    
 	</section>
   </>
   )
